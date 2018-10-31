4f983ee4d5a9d6b7c548aab38e7bb2d3274930e0
Beatriz <bia_nonato@icloud.com>
2018-10-31 15:09:19 -0300

    validar dados

--- fatura.py
+++ fatura.py
@@ -1,12 +1,28 @@
 repetir = 's'
 fatura = []
 total = 0
+validPreco = False
 
 while repetir == 's':
     produto = input('Digite o nome do produto: ')
-    preco = float(input('Digite o preço do produto: '))
+
+    while validPreco == False:
+        preco = input('Digite o preço do produto: ')
+        try:
+             preco = float(preco)
+             
+             if preco <= 0:
+                print('O preço precisar ser maior do que zero')
+             else:
+                  validPreco = True
+             
+        except:
+             print("Formato de preço inválido. Use apenas números e separe os centavos '.'.")
+             
+
     fatura.append([produto,preco])
-    total =+ preco
+    total += preco
+    validPreco = False
     repetir = input('Deseja comprar mais algum produto? (S para sim, N para não) ').lower()
 
 for i in fatura:
